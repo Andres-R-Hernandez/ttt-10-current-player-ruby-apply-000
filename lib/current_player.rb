5b45daf007ae3f43dd86fe7f54ea250b64914cbb
Andres Hernandez <andres.hernandez@utexas.edu>
2020-09-25 22:41:29 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   number_of_turns = 0
   board.each do |turns|
-    if turns != "" || turns != " "
+    if turns == "X" || turns = "O"
       number_of_turns += 1
     end
   end
